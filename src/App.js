066f828e37d0639050000c85929e208331d9a782
Michael G. Zajac <zajacmg@debian.dursyn01.synology.me>
2026-03-04 14:03:33 -0500

    Landing page component successfully completed.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Nav from './components/Nav';
+import Landing from './components/Landing';
 
 function App() {
   return (
     <div className="App">
       <Nav />
+      <Landing />
     </div>
   );
 }
