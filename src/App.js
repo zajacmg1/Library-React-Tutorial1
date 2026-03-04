50808da1084ea4bc7435148b0033ad7c725cd922
Michael G. Zajac <zajacmg@debian.dursyn01.synology.me>
2026-03-04 15:01:02 -0500

    Completing Highlights.jsx but receiving error:  npx eslint src/components/Highlights.jsx
    /home/zajacmg/SynologyDrive/FES/portfolio/library-react-tutorial1/src/components/Highlights.jsx
      14:18  error  'FontAwesomeIcon' is not defined  react/jsx-no-undef
      23:18  error  'FontAwesomeIcon' is not defined  react/jsx-no-undef
      32:18  error  'FontAwesomeIcon' is not defined  react/jsx-no-undef

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Nav from './components/Nav';
 import Landing from './components/Landing';
+import Highlights from './components/Highlights';
 
 function App() {
   return (
     <div className="App">
       <Nav />
       <Landing />
+      <Highlights />
     </div>
   );
 }
