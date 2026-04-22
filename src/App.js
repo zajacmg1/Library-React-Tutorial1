7bb204ff0ecaa051eaa013314ad0e02f129d22aa
Michael G. Zajac <zajacmg@debian.dursyn01.synology.me>
2026-04-22 16:08:44 -0400

    Discounted.jsx component successfully completed.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import Nav from './components/Nav';
 import Landing from './components/Landing';
 import Highlights from './components/Highlights';
 import Featured from './components/Featured';
+import Discounted from './components/Discounted';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Landing />
       <Highlights />
       <Featured />
+      <Discounted />
     </div>
   );
 }
