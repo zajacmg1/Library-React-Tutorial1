2ca7cd39cc7d09978e77b6a8e2d26665eb102a82
Michael G. Zajac <zajacmg@debian.dursyn01.synology.me>
2026-04-23 16:49:05 -0400

    Books Routing component page successfully completed.

--- src/App.js
+++ src/App.js
@@ -1,22 +1,19 @@
 import Nav from './components/Nav';
-import Landing from './components/Landing';
-import Highlights from './components/Highlights';
-import Featured from './components/Featured';
-import Discounted from './components/Discounted';
-import Explore from './components/Explore';
 import Footer from './components/Footer';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Books from './pages/books';
 
 function App() {
   return (
+    <Router>
     <div className="App">
       <Nav />
-      <Landing />
-      <Highlights />
-      <Featured />
-      <Discounted />
-      <Explore />
+      <Route path="/" exact component={Home} />
+      <Route path="/books" component={Books} />
       <Footer />
     </div>
+    </Router>
   );
 }
 
