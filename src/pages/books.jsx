ecbc9b840fb3792edd4d8a63fea9f44fca2bb491
Michael G. Zajac <zajacmg@debian.dursyn01.synology.me>
2026-04-23 17:44:54 -0400

    Filtering module successfully completed.

--- src/pages/books.jsx
+++ src/pages/books.jsx
@@ -1,7 +1,38 @@
-import { books } from "../data";
+import React, { useState } from "react";
 import Book from "../components/ui/Book";
 
-const Books = () => {
+const Books = ({ books: initialBooks }) => {
+  const [books, setBooks] = useState(initialBooks);
+
+  function filterBooks(filter) {
+    console.log(filter);
+    if (filter === "LOW_TO_HIGH") {
+      setBooks(
+        books
+          .slice()
+          .sort(
+            (a, b) =>
+              (a.salePrice || a.originalPrice) -
+              (b.salePrice || b.originalPrice),
+          ),
+      );
+    }
+    if (filter === "HIGH_TO_LOW") {
+      setBooks(
+        books
+          .slice()
+          .sort(
+            (a, b) =>
+              (b.salePrice || b.originalPrice) -
+              (a.salePrice || a.originalPrice),
+          ),
+      );
+    }
+    if (filter === "RATING") {
+        setBooks(books.slice().sort((a, b) => b.rating - a.rating));
+        }   
+    }
+
   return (
     <div id="books__body">
       <main id="books__main">
@@ -12,16 +43,22 @@ const Books = () => {
                 <h2 className="section__title books__header--title">
                   All Books
                 </h2>
-                <select id="filter" defaultValue="DEFAULT">
-                    <option value="DEFAULT" disabled>Sort</option>
-                    <option value="LOW_TO_HIGH">Price, Low to High</option>
-                    <option value="HIGH_TO_LOW">Price, High to Low</option>
-                    <option value="RATING">Rating</option>
+                <select
+                  id="filter"
+                  defaultValue="DEFAULT"
+                  onChange={(event) => filterBooks(event.target.value)}
+                >
+                  <option value="DEFAULT" disabled>
+                    Sort
+                  </option>
+                  <option value="LOW_TO_HIGH">Price, Low to High</option>
+                  <option value="HIGH_TO_LOW">Price, High to Low</option>
+                  <option value="RATING">Rating</option>
                 </select>
               </div>
               <div className="books">
                 {books.map((book) => (
-                    <Book book={book} key={book.id} />
+                  <Book book={book} key={book.id} />
                 ))}
               </div>
             </div>
